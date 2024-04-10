b797f239ac8cdc2c6b4e0c78dd875f65883bf95d
Cassie404 <103102535+Cassie404@users.noreply.github.com>
2024-04-11 03:37:29 +0800

    Add files via upload

---
+++ unlockCODE/code.js
@@ -0,0 +1,43 @@
+var Acode = document.getElementById('code');
+var submit = document.getElementById('submit');
+
+
+submit.onclick = function () {
+    switch (Acode.value.toLowerCase().trim()) {
+        case 'gotoisekai':
+            
+            break;
+        case 'w0rlds':
+            
+            break;
+        case 'gr1mmr':
+
+            break;
+        case 'r3v3rs':
+
+            break;
+        case 'ch4os7':
+
+            break;
+        case 'f4nt4s':
+
+            break;
+        case 'myth0s':
+
+            break;
+        case 'leg3nd':
+
+            break;
+        case 'kn1ght':
+
+            break;
+        case 'qu3st':
+
+            break;
+        case 'dr3ams':
+
+            break;
+        default:
+            alert("請輸入正確的code：）可從公會冒險餐點中取得~");
+    }
+}
