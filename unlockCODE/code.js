6c220d34aaf5d1f285573c5a197f98aeeb874e8d
Cassie404 <103102535+Cassie404@users.noreply.github.com>
2024-04-11 04:22:42 +0800

    Update code.js

--- unlockCODE/code.js
+++ unlockCODE/code.js
@@ -5,7 +5,7 @@ var submit = document.getElementById('submit');
 submit.onclick = function () {
     switch (Acode.value.toLowerCase().trim()) {
         case 'gotoisekai':
-            window.location.href = ("https://cassie404.github.io/tbbdetective.github.io/Day1/index.html");
+            location.replace("https://cassie404.github.io/tbbdetective.github.io/Day1/index.html");
             alert("Day1");
             break;
         case 'w0rlds':
