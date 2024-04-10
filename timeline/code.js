4111260b4cf1ffacd4e802fdb69786cd30c314df
Cassie404 <103102535+Cassie404@users.noreply.github.com>
2024-04-11 05:44:26 +0800

    Add files via upload

---
+++ timeline/code.js
@@ -0,0 +1,42 @@
+var t1 = document.getElementById('t1');
+var t2 = document.getElementById('t2');
+var t3 = document.getElementById('t3');
+var t4 = document.getElementById('t4');
+var t5 = document.getElementById('t5');
+var t6 = document.getElementById('t6');
+var t7 = document.getElementById('t7');
+var t8 = document.getElementById('t8');
+var t9 = document.getElementById('t9');
+var t10 = document.getElementById('t10');
+
+t1.onclick = function() {
+    // location.replace("");
+    alert('hi1');
+}
+t2.onclick = function() {
+    // location.replace("");
+}
+t3.onclick = function() {
+    // location.replace("");
+}
+t4.onclick = function() {
+    // location.replace("");
+}
+t5.onclick = function() {
+    // location.replace("");
+}
+t6.onclick = function() {
+    // location.replace("");
+}
+t7.onclick = function() {
+    // location.replace("");
+}
+t8.onclick = function() {
+    // location.replace("");
+}
+t9.onclick = function() {
+    // location.replace("");
+}
+t10.onclick = function() {
+    // location.replace("");
+}
