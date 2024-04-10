a30b54dd83873e29a7cd056015165108ec1f297e
Cassie404 <103102535+Cassie404@users.noreply.github.com>
2024-04-11 03:56:37 +0800

    Add files via upload

---
+++ Day1/data/slide3.js
@@ -0,0 +1,3 @@
+(function(){var loadHandler=window['sl_{8E808788-B94D-4E86-961E-9571BF7C1D27}'];loadHandler&&loadHandler(2, '<div id="spr0_24700534"><div id="spr1_24700534" class="kern slide"><img id="img1_24700534" src="data/img12.png" width="1440px" height="810px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_24700534" class="kern slide"><div id="spr3_24700534" aria-label="一張含有 文字, 信, 信封, 螢幕擷取畫面 的圖片\
+\
+自動產生的描述" role="group"><div id="spr4_24700534"><img id="img0_24700534" src="data/img20.jpg" width="1440" height="810" alt=""/></div></div></div></div>', '{"s":[]}');})();
